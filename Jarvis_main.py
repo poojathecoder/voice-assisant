83c6d4eb0eea9b56b207b9bafd4ea9b1fabb1c4e
Pooja Mohanty <84809468+poojathecoder@users.noreply.github.com>
2024-10-08 22:25:30 +0530

    Add files via upload

---
+++ Jarvis_main.py
@@ -0,0 +1,174 @@
+import datetime
+from email import message
+import webbrowser
+from numpy import tile
+import pyttsx3
+import speech_recognition
+import requests
+from bs4 import BeautifulSoup
+import os
+import pyautogui
+import random
+from plyer import notification
+from pygame import mixer
+import speedtest
+
+for i in range(3):
+    a = input("Enter Password to open Jarvis :- ")
+    pw_file = open("password.txt","r")
+    pw = pw_file.read()
+    pw_file.close()
+    if (a==pw):
+        print("WELCOME SIR ! PLZ SPEAK [Hello] TO LOAD ME UP")
+        break
+    elif (i==2 and a!=pw):
+        exit()
+
+    elif (a!=pw):
+        print("Try Again")
+
+
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id)
+rate = engine.setProperty("rate",170)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def takeCommand():
+    r = speech_recognition.Recognizer()
+    with speech_recognition.Microphone() as source:
+        print("Listening.....")
+        r.pause_threshold = 1
+        r.energy_threshold = 300
+        audio = r.listen(source,0,4)
+
+    try:
+        print("Understanding..")
+        query  = r.recognize_google(audio,language='en-in')
+        print(f"You Said: {query}\n")
+    except Exception as e:
+        print("Say that again")
+        return "None"
+    return query
+
+
+
+
+if __name__ == "__main__":
+    while True:
+        query = takeCommand().lower()
+        if "hello" in query:
+            from GreetMe import greetMe
+            greetMe()
+
+            while True:
+                query = takeCommand().lower()
+                if "go to sleep" in query:
+                    speak("Ok sir , You can call me anytime")
+                    break 
+                
+                
+
+                elif "change password" in query:
+                    speak("What's the new password")
+                    new_pw = input("Enter the new password\n")
+                    new_password = open("password.txt","w")
+                    new_password.write(new_pw)
+                    new_password.close()
+                    speak("Done sir")
+                    speak(f"Your new password is{new_pw}")
+
+
+                elif "open" in query:   #EASY METHOD
+                    query = query.replace("open","")
+                    query = query.replace("jarvis","")
+                    pyautogui.press("super")
+                    pyautogui.typewrite(query)
+                    pyautogui.sleep(2)
+                    pyautogui.press("enter")
+
+                elif "play a game" in query:
+                    from game import game_play
+                    game_play()
+
+                
+                
+
+                elif "hello" in query:
+                    speak("Hello sir, how are you ?")
+                elif "i am fine" in query:
+                    speak("that's great, sir")
+                elif "how are you" in query:
+                    speak("Perfect, sir")
+                elif "thank you" in query:
+                    speak("you are welcome, sir")
+                
+                
+                elif "open" in query:
+                    from Dictapp import openappweb
+                    openappweb(query)
+                elif "close" in query:
+                    from Dictapp import closeappweb
+                    closeappweb(query)
+
+
+                elif "google" in query:
+                    from SearchNow import searchGoogle
+                    searchGoogle(query)
+                elif "youtube" in query:
+                    from SearchNow import searchYoutube
+                    searchYoutube(query)
+                elif "wikipedia" in query:
+                    from SearchNow import searchWikipedia
+                    searchWikipedia(query)
+
+                
+
+                elif "temperature" in query or "weather" in query:
+                    city = ""
+                    if "in" in query:
+                        city = query.split("in")[-1].strip()
+                    else:
+                        city = input("Which city's temperature do you want to know? ").strip()
+
+                    search = f"temperature in {city}"
+                    url = f"https://www.google.com/search?q={search}"
+                    r = requests.get(url)
+                    data = BeautifulSoup(r.text, "html.parser")
+                    temp = data.find("div", class_="BNeawe").text
+                    speak(f"Current temperature in {city} is {temp}")
+
+
+                
+                           
+                elif "what is the time" in query:
+                    strTime = datetime.datetime.now().strftime("%H:%M")    
+                    speak(f"Sir, the time is {strTime}")
+                elif "finally sleep" in query:
+                    speak("Going to sleep,sir")
+                    exit()
+
+                
+
+                elif "shutdown system" in query:
+                    speak("Are You sure you want to shutdown")
+                    shutdown = input("Do you wish to shutdown your computer? (yes/no)")
+                    if shutdown == "yes":
+                        os.system("shutdown /s /t 1")
+
+                    elif shutdown == "no":
+                        break
+
+                
+
+
+
+
+                
+
+
+ 
